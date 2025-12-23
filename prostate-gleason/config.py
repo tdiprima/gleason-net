8b3370b755615fe2f3a9c1335a59d03586acacf6
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2025-12-23 14:14:19 -0500

    weighting=none, early stopping=accuracy

--- prostate-gleason/config.py
+++ prostate-gleason/config.py
@@ -53,7 +53,7 @@ CLASS_COUNTS = np.array([19695, 26816, 8461, 815], dtype=np.float32)
 #   "inverse_sqrt"      - Square root of inverse frequency (softer)
 #   "effective"         - Effective number of samples (Class-Balanced Loss paper)
 #   "focal"             - Use Focal Loss instead of CrossEntropy
-WEIGHTING_STRATEGY = "inverse_sqrt"  # <-- Change this to switch strategies
+WEIGHTING_STRATEGY = "none"  # <-- Change this to switch strategies
 
 # For "effective" strategy: beta parameter (0.9, 0.99, 0.999, 0.9999 are common)
 # Higher beta = more aggressive weighting toward minority classes
@@ -85,7 +85,7 @@ EARLY_STOPPING_PATIENCE = 5
 EARLY_STOPPING_MIN_DELTA = 0.0001
 
 # Metric to monitor: "accuracy", "f1", "loss"
-EARLY_STOPPING_METRIC = "f1"
+EARLY_STOPPING_METRIC = "accuracy"
 
 
 def compute_class_weights(strategy, class_counts, beta=0.999):
